f6a3ad5b89357cd50822d85856480b19d7144e08
Ackshaey Singh <ackshaey@gmail.com>
2015-11-17 14:20:42 -0800

    Added details navigation methods and classes for

--- ViewController/src/mobile/datacontrol/TodoTaskDC.java
+++ ViewController/src/mobile/datacontrol/TodoTaskDC.java
@@ -17,6 +17,12 @@ public class TodoTaskDC {
     
     private TodoTaskEntity[] allTodoTasks = null;
     
+    //Collection with single todo = record to edit in a MAF form. Providing a separate collection for 
+    //the selected employee allows implementation of a cancel-form strategy, which is that the collection
+    //data - and this the REST service - is only updated in case of a submit. If the user presses cancel 
+    //then nothing happens and the navigation returns to the calling page. 
+    private TodoTaskEntity[] editableTodoTask = new TodoTaskEntity[0];
+    
     public TodoTaskDC() {
         super();
     }
@@ -51,6 +57,39 @@ public class TodoTaskDC {
         this.allTodoTasks = todoTasks;
         providerChangeSupport.fireProviderRefresh("allTodoTasks");
     }
+    
+    public void setEditableTodoTask(TodoTaskEntity[] todoTaskToEdit){ 
+        //create a copy of the object passed for edit. Without copying the todoTask data we 
+        //would work on a object reference of the existing object, which makes it difficult 
+        //to undo the user edits
+        
+        ArrayList  todoTasksToEditList = new ArrayList();
+                
+        if(todoTaskToEdit.length >0){
+            todoTasksToEditList.add(todoTaskToEdit[0].clone());
+        }
+               
+        this.editableTodoTask = (TodoTaskEntity[]) todoTasksToEditList.toArray(new TodoTaskEntity[todoTasksToEditList.size()]);
+    }
+
+    public TodoTaskEntity[] getEditableTodoTask() {
+        return editableTodoTask;
+    }
+    
+    /**
+     * Called when preparing to navigate to the details of a to-do task
+     * @param id
+     */
+    public void prepareTodoTasksForDetails(Integer id){ 
+        String restURI = TodoTaskURIs.GetTodoTaskDetailsURI(id);
+        RestCallerUtil rcu = new RestCallerUtil();
+        String jsonArrayAsString = rcu.invokeREAD(restURI);
+        TodoTaskEntity[] task = JsonArrayToTodoTasksArray.getTodoTasksArray(jsonArrayAsString);
+        //no caching. allTodoTasks is always updated to the recent queried task
+        this.allTodoTasks = task;        
+        providerChangeSupport.fireProviderRefresh("allTodoTasks");       
+    };
+
     
     protected transient ProviderChangeSupport providerChangeSupport = new ProviderChangeSupport(this);
 
